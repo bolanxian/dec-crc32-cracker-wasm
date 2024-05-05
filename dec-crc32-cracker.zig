3fcf3d5c2123d65655d85be332849cde4d389beb
僠儖僲 <mahn@vip.qq.com>
2024-05-05 11:18:06 +0800

    已修改        dec-crc32-cracker.zig 已修改        main.zig

--- dec-crc32-cracker.zig
+++ dec-crc32-cracker.zig
@@ -12,9 +12,7 @@ pub export fn init(POLY: u32) usize {
                 j >> 1;
         }
         table[i] = j;
-    }
-    for (table, 0..256) |value, i| {
-        lastIndex[value >> 24] = @intCast(i);
+        lastIndex[j >> 24] = @intCast(i);
     }
     return @intFromPtr(&table);
 }
@@ -31,48 +29,42 @@ const Indexes = extern union {
     value: u32,
     bytes: [4]u8,
 };
-pub var indexes: Indexes = zeroes(Indexes);
+pub var indexes = zeroes(Indexes);
 pub export fn getIndexes(_crc: u32) u32 {
     var crc = _crc;
-    for ([_]u5{ 3, 2, 1, 0 }, [_]u5{ 0, 1, 2, 3 }) |i, j| {
+    var j: u5 = 0;
+    while (j < 4) : (j += 1) {
+        const i: u5 = 3 - j;
         const _crc_index = lastIndex[crc >> (i << 3)];
         indexes.bytes[j] = _crc_index;
         crc ^= table[_crc_index] >> (j << 3);
     }
     return indexes.value;
 }
-pub fn swap(comptime T: type, a: *T, b: *T) void {
-    const tmp: T = a.*;
-    a.* = b.*;
-    b.* = tmp;
-}
+
 pub fn itoa(num: u32, buf: *[10]u8) []const u8 {
-    var i: u8 = 0;
+    var i: u8 = buf.len;
     var tmp = num;
     while (true) {
+        i -= 1;
         buf[i] = @truncate((tmp % 10) + '0');
-        i += 1;
         tmp /= 10;
         if (tmp == 0) break;
     }
-    var j: u8 = 0;
-    while (j <= (i - 1) / 2) : (j += 1) {
-        swap(u8, &buf[i - 1 - j], &buf[j]);
-    }
-
-    return buf[0..i];
+    return buf[i..];
 }
 pub var str = zeroes([10]u8);
 pub export fn crc32_dec(i: u32) u32 {
     return crc32(itoa(i, &str));
 }
 pub fn check(_i: u32) i32 {
-    var crc: u32 = crc32(itoa(_i, &str));
+    var crc: u32 = crc32_dec(_i);
     if (crc_index != indexes.bytes[3]) {
         return -1;
     }
     var low: u12 = 0;
-    for ([_]u8{ 2, 1, 0 }) |i| {
+    var i: u8 = 2;
+    while (true) : (i -= if (i == 0) break else 1) {
         const num: u8 = @as(u8, @truncate(crc)) ^ indexes.bytes[i];
         if (num < '0' or num > '9') {
             return -1;
--- main.zig
+++ main.zig
@@ -1,4 +1,4 @@
-const zeroes = @import("std").mem.zeroes;
+const std = @import("std");
 const cracker = @import("./dec-crc32-cracker.zig");
 const init = cracker.init;
 const getIndexes = cracker.getIndexes;
@@ -15,18 +15,18 @@ pub fn concat(i: i32, j: i32) i64 {
     return ret;
 }
 
+const POLY = 0xEDB88320;
+const begin: i32 = 1;
+const end: i32 = 2000000;
+
 test "!" {
-    const std = @import("std");
     const expect = std.testing.expect;
-
-    _ = init(0xEDB88320);
-
+    _ = init(POLY);
     const crc: u32 = crc32_dec(1234567890);
     const _indexes = getIndexes(crc);
-    const begin: i32 = 1;
-    const end: i32 = 2000000;
 
     var i = begin;
+    var count: u8 = 0;
     while (i < end) : (i += 1) {
         i = findIndex(_indexes, i, end);
         if (i < 0) {
@@ -37,21 +37,20 @@ test "!" {
             729601752, 1234567890 => true,
             else => false,
         });
+        count += 1;
     }
+    try expect(count == 2);
     try expect(~crc == 0x261DAEE5);
 }
 
 pub fn main() !void {
-    const std = @import("std");
-
-    var _gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const gpa = _gpa.allocator();
-    const args = try std.process.argsAlloc(gpa);
-    defer std.process.argsFree(gpa, args);
-
-    _ = init(0xEDB88320);
-
-    for (args[1..]) |arg| {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    var args = try std.process.argsWithAllocator(alloc);
+    defer args.deinit();
+    _ = init(POLY);
+    _ = args.skip();
+    while (args.next()) |arg| {
         var crc: u32 = 0;
         for (arg) |char| {
             crc = crc << 4 | try switch (char) {
@@ -63,8 +62,6 @@ pub fn main() !void {
         }
         crc = ~crc;
         const _indexes = getIndexes(crc);
-        const begin: i32 = 1;
-        const end: i32 = 2000000;
         var i = begin;
         while (i < end) : (i += 1) {
             i = findIndex(_indexes, i, end);
@@ -72,7 +69,6 @@ pub fn main() !void {
                 break;
             }
             const last = checkIndex(i, _indexes);
-
             std.debug.print("crc32(\"{d}\") = {s}\n", .{ concat(i, last), arg });
         }
     }
