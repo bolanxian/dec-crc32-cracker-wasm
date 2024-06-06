00ce49f78df485b7c2ed15667fd5ca701433288f
僠儖僲 <mahn@vip.qq.com>
2024-06-06 16:56:56 +0800

    已修改        dec-crc32-cracker.zig

--- dec-crc32-cracker.zig
+++ dec-crc32-cracker.zig
@@ -1,7 +1,5 @@
-const zeroes = @import("std").mem.zeroes;
-
-var table = zeroes([256]u32);
-var lastIndex = zeroes([256]u8);
+var table: [256]u32 = undefined;
+var lastIndex: [256]u8 = undefined;
 pub export fn init(POLY: u32) usize {
     for (0..256) |i| {
         var j: u32 = @intCast(i);
@@ -16,7 +14,7 @@ pub export fn init(POLY: u32) usize {
     }
     return @intFromPtr(&table);
 }
-pub var crc_index: u8 = 0;
+var crc_index: u8 = 0;
 pub fn crc32(buf: []const u8) u32 {
     var crc = ~@as(u32, 0);
     for (buf) |value| {
@@ -25,21 +23,18 @@ pub fn crc32(buf: []const u8) u32 {
     }
     return crc;
 }
-const Indexes = extern union {
-    value: u32,
-    bytes: [4]u8,
-};
-pub var indexes = zeroes(Indexes);
+
+var indexes: [4]u8 = undefined;
 pub export fn getIndexes(_crc: u32) u32 {
     var crc = _crc;
     var j: u5 = 0;
     while (j < 4) : (j += 1) {
         const i: u5 = 3 - j;
         const _crc_index = lastIndex[crc >> (i << 3)];
-        indexes.bytes[j] = _crc_index;
+        indexes[j] = _crc_index;
         crc ^= table[_crc_index] >> (j << 3);
     }
-    return indexes.value;
+    return @bitCast(indexes);
 }
 
 pub fn itoa(num: u32, buf: *[10]u8) []const u8 {
@@ -53,33 +48,33 @@ pub fn itoa(num: u32, buf: *[10]u8) []const u8 {
     }
     return buf[i..];
 }
-pub var str = zeroes([10]u8);
+var str: [10]u8 = undefined;
 pub export fn crc32_dec(i: u32) u32 {
     return crc32(itoa(i, &str));
 }
-pub fn check(_i: u32) i32 {
+fn check(_i: u32) i32 {
     var crc: u32 = crc32_dec(_i);
-    if (crc_index != indexes.bytes[3]) {
+    if (crc_index != indexes[3]) {
         return -1;
     }
     var low: u12 = 0;
     var i: u8 = 2;
     while (true) : (i -= if (i == 0) break else 1) {
-        const num: u8 = @as(u8, @truncate(crc)) ^ indexes.bytes[i];
+        const num: u8 = @as(u8, @truncate(crc)) ^ indexes[i];
         if (num < '0' or num > '9') {
             return -1;
         }
         low = (low << 4) | (num - '0');
-        crc = (crc >> 8) ^ table[indexes.bytes[i]];
+        crc = (crc >> 8) ^ table[indexes[i]];
     }
     return low;
 }
 pub export fn checkIndex(i: i32, indexes_value: u32) i32 {
-    indexes.value = indexes_value;
+    indexes = @bitCast(indexes_value);
     return check(@intCast(i));
 }
 pub export fn findIndex(indexes_value: u32, begin: i32, end: i32) i32 {
-    indexes.value = indexes_value;
+    indexes = @bitCast(indexes_value);
     var i = begin;
     while (i < end) : (i += 1) {
         const low = check(@intCast(i));
